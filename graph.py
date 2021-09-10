b5b200c82f8e139e3d0e992e5cafad421de15e64
Thea Barnes <thea@tsbarnes.com>
2021-09-10 15:32:55 -0400

    Improvements for graph.py

--- graph.py
+++ graph.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-
 from time import sleep
-from gpiozero import LoadAverage
 
+from gpiozero import LoadAverage
 from microdotphat import set_col, show, clear
 
 
@@ -13,28 +12,40 @@ Plots load average across the screen in a bar graph.
 Press Ctrl+C to exit.
 """)
 
-graph = []
-filled = True
 
-while True:
-    clear()
-    print(LoadAverage(minutes=1).load_average)
-    graph += [int(LoadAverage(minutes=1).load_average)]
-    while len(graph) > 45:
-        graph.pop(0)
+class Graph:
+    graph = []
+    filled = True
 
-    for x, val in enumerate(graph):
-        if filled:
-            set_col(x + (45-len(graph)), [
-                0,
-                0b1000000,
-                0b1100000,
-                0b1110000,
-                0b1111000,
-                0b1111100,
-                0b1111110,
-                0b1111111][val])
-        else:
-            set_col(x, 1 << (7-val))
+    def iterate_loop(self):
+        clear()
+        print(LoadAverage(minutes=1).load_average)
+        self.graph += [int(LoadAverage(minutes=1).load_average)]
+        while len(self.graph) > 45:
+            self.graph.pop(0)
 
-    show()
+        for x, val in enumerate(self.graph):
+            if self.filled:
+                set_col(x + (45-len(self.graph)), [
+                    0,
+                    0b1000000,
+                    0b1100000,
+                    0b1110000,
+                    0b1111000,
+                    0b1111100,
+                    0b1111110,
+                    0b1111111][val])
+            else:
+                set_col(x, 1 << (7-val))
+
+        show()
+
+    def main_loop(self):
+        while True:
+            self.iterate_loop()
+            sleep(1)
+
+
+if __name__ == '__main__':
+    graph = Graph()
+    graph.main_loop()

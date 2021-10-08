59c282869b8e2c08458724a3534fd1e1f491d4bb
Thea Barnes <thea@tsbarnes.com>
2021-10-08 02:45:44 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -4,15 +4,17 @@ import logging
 import posix_ipc
 
 
+logger = logging.getLogger("cli")
+
 if len(sys.argv) < 2:
-    logging.error("No command specified")
+    logger.error("No command specified")
     exit(1)
 
 try:
     mq = posix_ipc.MessageQueue("/microdotphat_ipc")
     mq.block = False
 except posix_ipc.PermissionsError:
-    print("couldn't open message queue")
+    logger.error("couldn't open message queue")
     exit(1)
 
 command_line = " ".join(sys.argv[1:])
